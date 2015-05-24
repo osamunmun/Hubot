d90cbdc9e9e3e4fbd5b90e8c281c635321ae569a
Osamu Takayasu <osamu.takayasu@gmail.com>
2015-05-24 17:58:49 +0900

    Fix aws cli command.

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 set -ex
 
-SECURITY_GROUP_NAME="circle_ci"
 IP=`curl -s ifconfig.me`
 
-trap "aws ec2 revoke-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-name ${SECURITY_GROUP_NAME} --protocol tcp --port 22 --cidr ${IP}/32
+trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 cd ansible; ansible-playbook -i hosts hubot.yml
