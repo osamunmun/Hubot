d36944204bf9d42034aff821362e33217df6c36d
osamunmun <osamu.takayasu@gmail.com>
2015-05-24 21:53:42 +0900

    Fix security group id environment var name.

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ set -ex
 
 IP=`curl -s ifconfig.me`
 
-trap "aws ec2 revoke-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
-aws ec2 authorize-security-group-ingress --group-id ${SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
+trap "aws ec2 revoke-security-group-ingress --group-id ${AWS_SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32" 0 1 2 3 15
+aws ec2 authorize-security-group-ingress --group-id ${AWS_SECURITY_GROUP_ID} --protocol tcp --port 22 --cidr ${IP}/32
 cd ansible; ansible-playbook -i hosts hubot.yml --vault-password-file ~/vault.txt
